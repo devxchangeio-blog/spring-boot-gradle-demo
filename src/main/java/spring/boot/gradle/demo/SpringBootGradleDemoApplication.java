c1cebdda5775711f27f1889b7957f17f37e3b3aa
karthikeyan <karthy86@live.com>
2016-02-22 01:06:28 -0500

    Initial Commit
    Initial Commit

---
+++ src/main/java/spring/boot/gradle/demo/SpringBootGradleDemoApplication.java
@@ -0,0 +1,12 @@
+package spring.boot.gradle.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGradleDemoApplication.class, args);
+	}
+}
